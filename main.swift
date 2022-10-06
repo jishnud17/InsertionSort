f23273b83df87d6a8b13af10497b6e1b38e77bd8
Jishnu Devarapalli <srijishnu.devarapalli@student.allenisd.org>
2022-10-05 21:50:25 -0500

    first commit

---
+++ main.swift
@@ -0,0 +1,36 @@
+var integers:[String] = ["cat", "apple"] 
+var last_position = integers.count 
+var sap = true
+var key = ""
+var j = 0
+    for i in 1..<last_position {
+        key = integers[i]
+        j = i - 1
+        while j >= 0 && integers[j] > key  {
+            integers.swapAt(j, j+1)
+            j -= 1
+        }
+        integers[j + 1] = key
+      
+    }
+    print(integers)    
+    
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
